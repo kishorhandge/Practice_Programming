4f245f4eeb238ad189457d12d95ecc3932053c1a
kishorhandge <kishorhandge29@gmail.com>
2026-03-30 23:02:10 +0530

    Add files via upload

---
+++ SinglyLL/CheckAllPositive.c
@@ -0,0 +1,99 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+
+struct node
+{
+    int data;
+    struct node *next;
+};
+
+typedef struct node NODE;
+typedef struct node *PNODE;
+typedef struct node **PPNODE;
+
+
+void InsertFirst(PPNODE first,int no)
+{
+    PNODE newn = NULL;
+
+    newn = (PNODE)malloc(sizeof(NODE));
+
+    newn->data = no;
+    newn->next = NULL;
+
+    if(*first == NULL)
+    {
+        (*first) = newn;
+    }
+    else
+    {
+        newn->next = (*first);
+        (*first) = newn;
+    }
+}
+
+void Display(PNODE first)
+{
+    while(first != NULL)
+    {
+        printf("| %d |->",first->data);
+        first = first->next;
+    }
+    printf("NULL\n");
+}
+
+bool CheckAllPositive(PNODE first)
+{
+    PNODE temp = NULL;
+    
+    temp = (first);
+
+    bool bFlag = true;
+
+    while(temp != NULL)
+    {
+        if((temp->data) < 0 )
+        {
+            bFlag = false;
+            break;
+        }
+ 
+        temp =  temp->next;
+    }
+
+    return bFlag; 
+
+}
+
+
+int main()
+{
+    PNODE head = NULL;
+    int iRet = 0;
+
+    InsertFirst(&head,51);
+    InsertFirst(&head,40);
+    InsertFirst(&head,31);
+    InsertFirst(&head,6);
+    InsertFirst(&head,-28);
+    InsertFirst(&head,20);
+    InsertFirst(&head,-21);
+    InsertFirst(&head,11);
+    
+    Display(head);
+
+    iRet = CheckAllPositive(head);
+
+    if(iRet == true)
+    {
+        printf("All Elements are positive:\n");
+    }
+    else
+    {
+        printf("All Element are not positive:\n");
+    }
+   
+
+    return 0;
+}
---
+++ SinglyLL/CheckPerfect.c
@@ -0,0 +1,109 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+
+struct node
+{
+    int data;
+    struct node *next;
+};
+
+typedef struct node NODE;
+typedef struct node *PNODE;
+typedef struct node **PPNODE;
+
+
+void InsertFirst(PPNODE first,int no)
+{
+    PNODE newn = NULL;
+
+    newn = (PNODE)malloc(sizeof(NODE));
+
+    newn->data = no;
+    newn->next = NULL;
+
+    if(*first == NULL)
+    {
+        (*first) = newn;
+    }
+    else
+    {
+        newn->next = (*first);
+        (*first) = newn;
+    }
+}
+
+void Display(PNODE first)
+{
+    while(first != NULL)
+    {
+        printf("| %d |->",first->data);
+        first = first->next;
+    }
+    printf("NULL\n");
+}
+
+int CheckPerfect(int iNo)
+{
+    int iCnt = 1;
+    int iSum = 0;
+
+    for(iCnt = 1;iCnt <= (iNo/2);iCnt++)
+    {
+        if(iNo % iCnt == 0)
+        {
+            iSum = iSum + iCnt;
+        }
+    }
+
+    if(iSum == iNo)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+void DisplayPerfect(PNODE first)
+{
+    PNODE temp = NULL;
+    
+    temp = (first);
+
+    printf("Perfect Elements are:\n ");
+
+    while(temp != NULL)
+    {
+        if(CheckPerfect(temp->data))
+        {
+            printf("| %d |->",temp->data);
+        }
+
+        temp =  temp->next;
+    }
+
+    printf("\n");
+}
+
+ 
+
+int main()
+{
+    PNODE head = NULL;
+    int iRet = 0;
+
+    InsertFirst(&head,51);
+    InsertFirst(&head,41);
+    InsertFirst(&head,31);
+    InsertFirst(&head,6);
+    InsertFirst(&head,28);
+    InsertFirst(&head,21);
+    InsertFirst(&head,11);
+    
+    Display(head);
+
+    DisplayPerfect(head);
+
+    return 0;
+}
