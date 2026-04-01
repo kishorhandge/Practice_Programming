a71a5a792f528d96f168280c2deec4c7faa0cd07
kishorhandge <kishorhandge29@gmail.com>
2026-04-01 23:10:27 +0530

    Add files via upload

---
+++ SinglyLL/DisplayCount.c
@@ -0,0 +1,93 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+
+struct node
+{
+    int data;
+    struct node *next;
+};
+
+typedef struct node NODE;
+typedef struct node *PNODE;
+typedef struct node **PPNODE;
+
+
+void InsertFirst(PPNODE first,int no)
+{
+    PNODE newn = NULL;
+
+    newn = (PNODE)malloc(sizeof(NODE));
+
+    newn->data = no;
+    newn->next = NULL;
+
+    if(*first == NULL)
+    {
+        (*first) = newn;
+    }
+    else
+    {
+        newn->next = (*first);
+        (*first) = newn;
+    }
+}
+
+void Display(PNODE first)
+{
+    while(first != NULL)
+    {
+        printf("| %d |->",first->data);
+        first = first->next;
+    }
+    printf("NULL\n");
+}
+
+int DisplayCount(PNODE first,int iPos)
+{
+    PNODE temp = NULL;
+    
+    temp = (first);
+
+    int pos = 1;
+
+    int iCount = 0;
+
+    while(temp != NULL)
+    {
+        if(pos % iPos == 0)
+        {
+            iCount++;
+        }
+
+        pos++;
+        temp =  temp->next;
+    }
+    return iCount;
+
+}
+
+int main()
+{
+    PNODE head = NULL;
+
+    int iValue = 0;
+    int iRet = 0;
+
+    InsertFirst(&head,51);
+    InsertFirst(&head,41);
+    InsertFirst(&head,31);
+    InsertFirst(&head,21);
+    InsertFirst(&head,11);
+    
+    Display(head);
+
+    printf("Enter the position:\n");
+    scanf("%d",&iValue);
+
+    iRet = DisplayCount(head,iValue);
+
+    printf("Number of node at given position is : %d\n",iRet);
+
+    return 0;
+}
