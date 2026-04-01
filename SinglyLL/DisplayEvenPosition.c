027df160dd7db2bfd713832535bc259e9a03532a
kishorhandge <kishorhandge29@gmail.com>
2026-04-01 23:10:57 +0530

    Add files via upload

---
+++ SinglyLL/DisplayEvenPosition.c
@@ -0,0 +1,85 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+
+struct node
+{
+    int data;
+    struct node *next;
+};
+
+typedef struct node NODE;
+typedef struct node *PNODE;
+typedef struct node **PPNODE;
+
+
+void InsertFirst(PPNODE first,int no)
+{
+    PNODE newn = NULL;
+
+    newn = (PNODE)malloc(sizeof(NODE));
+
+    newn->data = no;
+    newn->next = NULL;
+
+    if(*first == NULL)
+    {
+        (*first) = newn;
+    }
+    else
+    {
+        newn->next = (*first);
+        (*first) = newn;
+    }
+}
+
+void Display(PNODE first)
+{
+    while(first != NULL)
+    {
+        printf("| %d |->",first->data);
+        first = first->next;
+    }
+    printf("NULL\n");
+}
+
+void DisplayEvenPosition(PNODE first)
+{
+    PNODE temp = NULL;
+    
+    temp = (first);
+    int iPos = 1;
+
+    while(temp != NULL)
+    {
+        if(iPos % 2 == 0)
+        {
+            printf("| %d |->",(temp->data));
+        }
+        iPos+=1;
+        temp =  temp->next;
+    }
+    printf("NULL\n");
+
+}
+
+
+int main()
+{
+    PNODE head = NULL;
+    bool bRet = false;
+
+    InsertFirst(&head,51);
+    InsertFirst(&head,40);
+    InsertFirst(&head,31);
+    InsertFirst(&head,21);
+    InsertFirst(&head,11);
+    InsertFirst(&head,18);
+    InsertFirst(&head,8);
+    
+    Display(head);
+
+    DisplayEvenPosition(head);
+
+    return 0;
+}
