4e9924b8c9080f1c9ea77e0c054e091133364da9
kishorhandge <kishorhandge29@gmail.com>
2026-04-05 22:20:46 +0530

    Add files via upload

---
+++ SinglyLL/Maximum.c
@@ -0,0 +1,89 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+
+struct node
+{
+    int data;
+    struct node *next;
+};
+
+typedef struct node NODE;
+typedef struct node *PNODE;
+typedef struct node **PPNODE;
+
+
+void InsertFirst(PPNODE first,int no)
+{
+    PNODE newn = NULL;
+
+    newn = (PNODE)malloc(sizeof(NODE));
+
+    newn->data = no;
+    newn->next = NULL;
+
+    if(*first == NULL)
+    {
+        (*first) = newn;
+    }
+    else
+    {
+        newn->next = (*first);
+        (*first) = newn;
+    }
+}
+
+void Display(PNODE first)
+{
+    while(first != NULL)
+    {
+        printf("| %d |->",first->data);
+        first = first->next;
+    }
+    printf("NULL\n");
+}
+
+int Maximum(PNODE first)
+{
+    PNODE temp = NULL;
+    
+    temp = (first);
+
+    int iMax = temp->data;
+
+    while(temp != NULL)
+    {
+        if((temp->data) > iMax)
+        {
+            iMax = temp->data;
+        }
+
+        temp =  temp->next;
+    }
+
+    return iMax;
+}
+
+
+int main()
+{
+    PNODE head = NULL;
+    int iRet = 0;
+
+    InsertFirst(&head,51);
+    InsertFirst(&head,41);
+    InsertFirst(&head,31);
+    InsertFirst(&head,6);
+    InsertFirst(&head,28);
+    InsertFirst(&head,17);
+    InsertFirst(&head,21);
+    InsertFirst(&head,11);
+    
+    Display(head);
+
+    iRet = Maximum(head);
+
+    printf("Maximum element from list is : %d\n",iRet);
+
+    return 0;
+}
---
+++ SinglyLL/Minimum.c
@@ -0,0 +1,89 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+
+struct node
+{
+    int data;
+    struct node *next;
+};
+
+typedef struct node NODE;
+typedef struct node *PNODE;
+typedef struct node **PPNODE;
+
+
+void InsertFirst(PPNODE first,int no)
+{
+    PNODE newn = NULL;
+
+    newn = (PNODE)malloc(sizeof(NODE));
+
+    newn->data = no;
+    newn->next = NULL;
+
+    if(*first == NULL)
+    {
+        (*first) = newn;
+    }
+    else
+    {
+        newn->next = (*first);
+        (*first) = newn;
+    }
+}
+
+void Display(PNODE first)
+{
+    while(first != NULL)
+    {
+        printf("| %d |->",first->data);
+        first = first->next;
+    }
+    printf("NULL\n");
+}
+
+int Minimum(PNODE first)
+{
+    PNODE temp = NULL;
+    
+    temp = (first);
+
+    int iMin = temp->data;
+
+    while(temp != NULL)
+    {
+        if((temp->data) < iMin)
+        {
+            iMin = temp->data;
+        }
+
+        temp =  temp->next;
+    }
+
+    return iMin;
+}
+
+
+int main()
+{
+    PNODE head = NULL;
+    int iRet = 0;
+
+    InsertFirst(&head,51);
+    InsertFirst(&head,41);
+    InsertFirst(&head,31);
+    InsertFirst(&head,6);
+    InsertFirst(&head,28);
+    InsertFirst(&head,17);
+    InsertFirst(&head,21);
+    InsertFirst(&head,11);
+    
+    Display(head);
+
+    iRet = Minimum(head);
+
+    printf("Minimum element from list is : %d\n",iRet);
+
+    return 0;
+}
