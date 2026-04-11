e1d6fad9487539d184eb0672dc9ec0ec496f677f
kishorhandge <kishorhandge29@gmail.com>
2026-04-11 23:05:35 +0530

    Add files via upload

---
+++ DoublyLL/Digit.c
@@ -0,0 +1,87 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+
+struct node
+{
+    int data;
+    struct node *next;
+    struct node *prev;
+};
+
+typedef struct node NODE;
+typedef struct node *PNODE;
+typedef struct node **PPNODE;
+
+
+void InsertFirst(PPNODE first,int no)
+{
+    PNODE newn = NULL;
+
+    newn = (PNODE)malloc(sizeof(NODE));
+
+    newn->data = no;
+    newn->next = NULL;
+    newn->prev = NULL;
+
+    if(*first == NULL)
+    {
+        (*first) = newn;
+    }
+    else
+    {
+        newn->next = (*first);
+        (*first)->prev = newn;
+        (*first) = newn;
+    }
+}
+ 
+void Display(PNODE first)
+{
+    while(first != NULL)
+    {
+        printf("| %d |->",first->data);
+        first = first->next;
+    }
+    printf("NULL\n");
+}
+
+void Digit(PNODE first)
+{
+    PNODE temp = NULL;
+
+    temp = first;
+
+    int iDigit = 0;
+
+    while(temp != NULL)
+    {
+        while((temp->data) != 0)
+        {
+            iDigit = (temp->data) % 10;
+            printf("%d ",iDigit);
+            temp->data = (temp->data) /10;
+        }
+        temp = temp->next;
+    }
+}
+ 
+int main()
+{
+    PNODE head = NULL;
+    int iRet = 0;
+
+    InsertFirst(&head,51);
+    InsertFirst(&head,41);
+    InsertFirst(&head,31);
+    InsertFirst(&head,28);
+    InsertFirst(&head,21);
+    InsertFirst(&head,11);
+    InsertFirst(&head,6);
+
+    Display(head);
+
+    Digit(head);
+
+    return 0;
+}
